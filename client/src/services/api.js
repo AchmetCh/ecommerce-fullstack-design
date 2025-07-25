2a2e8cdc9eaa6e8ce3100348dd6a3542f0898023
Achmet <gigsakos@gmail.com>
2025-07-25 20:49:01 +0300

    delte folder server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8001/api';
+const API_URL = 'http://localhost:8000/api';
 
 export const getProducts = async () => {
     return await axios.get(`${API_URL}/products`);
