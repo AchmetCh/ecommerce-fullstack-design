1e4e1a7e52e670fc14edef4775ff07a86d900225
Achmet <gigsakos@gmail.com>
2025-08-07 20:49:16 +0300

    change api

--- client/src/components/services/Api.jsx
+++ client/src/components/services/Api.jsx
@@ -1,7 +1,7 @@
 
 
-export const backendApi = 'http://localhost:8000/api'
+// export const backendApi = 'http://localhost:8000/api'
 
-// export const backendApi = 'https://ecombackend.myrender.eu/api'
+export const backendApi = 'https://ecombackend.myrender.eu/api'
 
 
